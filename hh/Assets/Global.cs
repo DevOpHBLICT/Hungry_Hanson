3bf58b652a3fbcff54d20054eb941152e6e8ea18
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-03-08 09:18:27 +0000

    'sfds'

---
+++ hh/Assets/Global.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour {
+
+
+	public static int sc;
+	// Use this for initialization
+	void Start () {
+		sc = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
